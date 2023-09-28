c5eec8bc8099b40210c40ec23160ca1eae119e35
Yoones Khoshghadam <yooneskh@gmail.com>
2023-09-28 11:00:31 +0330

    feat: fill and solid variants

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -5,12 +5,78 @@
 
 <template>
   <div>
-    <button class="btn interactive bg-red">
 
-      <i class="i-heroicons-plus"></i>
-      Hello
-      <i class="i-heroicons-user"></i>
+    <div class="pa-3">
 
-    </button>
+      <div class="text-xl font-bold">
+        Fill
+      </div>
+
+      <div class="flex gap-2 mt-3">
+
+        <u-btn
+          icon="i-heroicons-user"
+          label="Welcome Here"
+          class="fill"
+        />
+
+        <u-btn
+          icon="i-heroicons-user"
+          label="Welcome Here"
+          class="fill color-blue"
+        />
+  
+        <u-btn
+          icon="i-heroicons-user"
+          label="Welcome Here"
+          class="fill color-orange"
+        />
+
+        <u-btn
+          icon="i-heroicons-user"
+          label="Welcome Here"
+          class="fill color-green-500"
+        />
+
+      </div>
+
+    </div>
+
+    <div class="pa-3">
+
+      <div class="text-xl font-bold">
+        Outline
+      </div>
+
+      <div class="flex gap-2 mt-3">
+
+        <u-btn
+          icon="i-heroicons-user"
+          label="Welcome Here"
+          class="outline"
+        />
+
+        <u-btn
+          icon="i-heroicons-user"
+          label="Welcome Here"
+          class="outline color-blue"
+        />
+  
+        <u-btn
+          icon="i-heroicons-user"
+          label="Welcome Here"
+          class="outline color-orange"
+        />
+
+        <u-btn
+          icon="i-heroicons-user"
+          label="Welcome Here"
+          class="outline color-green-500"
+        />
+
+      </div>
+
+    </div>
+
   </div>
 </template>
--- uno.config.js
+++ uno.config.js
@@ -1,5 +1,4 @@
 import { defineConfig, presetWind, presetIcons } from 'unocss'
-import { shortcuts } from './lib/shortcuts';
 
 
 export default defineConfig({
@@ -7,5 +6,37 @@ export default defineConfig({
     presetWind(),
     presetIcons(),
   ],
-  shortcuts,
+  rules: [
+    [
+      /^color-(\w+)-?(\d+)?$/,
+      ([ _, color, variant ], { theme }) => ({
+        '--u-color': theme.colors[color]?.[variant || 'DEFAULT'] || color
+      }),
+    ],
+  ],
+  shortcuts: [
+    {
+      'btn': `
+        px-2 py-1 rounded
+        flex gap-1 items-center
+        relative overflow-hidden
+        interactive
+        color-dark
+      `,
+      'interactive': `
+        after:absolute after:inset-0 after:content-[""] after:bg-[var(--u-color)] after:opacity-0
+        after:transition after:duration-150
+        hover:after:opacity-20 active:after:opacity-30
+      `,
+      'outline': `
+        bg-transparent
+        border-2px border-[var(--u-color)]
+        text-[var(--u-color)]
+      `,
+      'fill': `
+        bg-[var(--u-color)]
+        text-white
+      `,
+    },
+  ]
 });
