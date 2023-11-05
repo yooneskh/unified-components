087fbf6a8c13f07e24c6a35121ed5c9a4e5b432e
Yoones Khoshghadam <yooneskh@gmail.com>
2023-11-06 00:09:08 +0330

    feat (styles): add variants

--- uno.config.js
+++ uno.config.js
@@ -1,6 +1,21 @@
 import { defineConfig, presetWind, presetIcons } from 'unocss';
 
 
+// const colors = [
+//   'red',
+//   'green',
+//   'blue',
+//   'teal',
+//   'black',
+//   'white',
+//   'dark',
+//   'light',
+//   'primary',
+//   'secondary',
+//   'success',
+//   'error',
+// ];
+
 export default defineConfig({
   presets: [
     presetWind(),
@@ -10,9 +25,9 @@ export default defineConfig({
   ],
   rules: [
     [
-      /^color-(\w+)-?(\d+)?$/,
-      ([ _, color, variant ], { theme }) => ({
-        '--u-color': theme.colors[color]?.[variant || 'DEFAULT'] || color
+      /^color-(\w+)$/,
+      ([ _, color ]) => ({
+        '--u-color': `var(--color-${color})`
       }),
     ],
   ],
@@ -23,20 +38,39 @@ export default defineConfig({
         flex gap-1.5 items-center shrink-0
         relative overflow-hidden
         interactive
-        color-dark
       `,
       'interactive': `
         transition-transform hover:scale-98 active:scale-95
+      `,
+      'fill': `
+        bg-[var(--u-color)]
+        text-white
       `,
       'outline': `
         bg-transparent
         border-2px border-[var(--u-color)]
         text-[var(--u-color)]
       `,
-      'fill': `
-        bg-[var(--u-color)]
-        text-white
+      'soft': `
+        bg-transparent
+        text-[var(--u-color)]
+        before:absolute before:inset-0 before:content-[""] before:bg-[var(--u-color)] before:opacity-15
+      `,
+      'ghost': `
+        bg-transparent
+        text-[var(--u-color)]
+        before:absolute before:inset-0 before:content-[""] before:bg-[var(--u-color)] before:opacity-0
+        before:transition before:duration-150
+        hover:before:opacity-20 active:before:opacity-30
+      `,
+      'link': `
+        bg-transparent
+        text-[var(--u-color)]
+        hover:underline
       `,
     },
   ],
+  // safelist: [
+  //   ...colors.map(it => `color-${it}`),
+  // ]
 });
