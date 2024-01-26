6fe02c77ef9b331890cdfbdaa1a8e0ca9f84d8e9
Yoones Khoshghadam <yooneskh@gmail.com>
2024-01-26 20:44:41 +0330

    chore (packages): update

---
+++ playground/pages/index.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+/* page */
+
+definePageMeta({
+  name: 'general.home',
+});
+
+useHead({
+  title: 'Home',
+});
+
+
+/* template */
+
+</script>
+
+
+<template>
+  <div class="p-4 space-x-3">
+
+    <nuxt-link to="/buttons">
+      <u-btn>
+        Buttons
+      </u-btn>
+    </nuxt-link>
+
+    <nuxt-link to="/chips">
+      <u-btn>
+        Chips
+      </u-btn>
+    </nuxt-link>
+
+    <nuxt-link to="/dropdowns">
+      <u-btn>
+        Dropdowns
+      </u-btn>
+    </nuxt-link>
+
+    <nuxt-link to="/floatings">
+      <u-btn>
+        Floatings
+      </u-btn>
+    </nuxt-link>
+
+    <nuxt-link to="/tooltips">
+      <u-btn>
+        Tooltips
+      </u-btn>
+    </nuxt-link>
+
+  </div>
+</template>
